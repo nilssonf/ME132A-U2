da8a86d744ba58485e20462b8fa0aeb628483ad9
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-02-25 13:40:14 +0100

    Creating code base and database

---
+++ database.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let database = [
+
+    {
+        id: 1,
+        name: "Sjuan",
+        designer:"Arne Jacobsen",
+        year: 1955,
+        country: "Denmark",
+    },
+    {
+        id: 2,
+        name: "Flowerpot",
+        designer:"Verner Panton",
+        year: 1969,
+        country: "Denmark",
+    },
+    {
+        id: 3,
+        name: "Lilla Åland" ,
+        designer:"Carl Malmsten" ,
+        year: 1942,
+        country: "Sweden",
+    },
+    {
+        id: 4,
+        name: "Gatto Piccolo" ,
+        designer: "Achille & Pier Giacomo Castiglioni",
+        year: 1960,
+        country: "Italy",
+    },
+    {
+        id: 5,
+        name: "60 Pall" ,
+        designer:"Alvar Aalto" ,
+        year: 1933,
+        country: "Finland",
+    }
+   
+];
