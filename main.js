9e73e0349181c07dfa1e8e3e36f22b6de31ce21d
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-02-28 16:31:15 +0100

    Creating rendering functions using my database

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 "use strict";
 
+// Renders a furniture-objetct into an HTML element
+function renderFurniture(furniture){
+    let div = document.createElement("div");
+    div.classList.add("furniture");
+    div.id = furniture.id;
+
+    div.innerHTML = `
+        <div> ${furniture.name} </div>
+        <div> ${furniture.designer} </div>
+        <div> ${furniture.year} </div>
+        <div> ${furniture.country} </div>
+        <button type="button">Remove</button> 
+    `;
+
+    return div;
+}
+
+// Renders an array of furnitures into the HTML
+function renderFurnitures(furnitures){
+    let furnituresElement = document.getElementById("furnitures");
+    furnituresElement.innerHTML = "";
+
+    //Look through the furnitures and insert their HTML
+    for (let furniture of furnitures){
+        let furnitureElement = renderFurniture(furniture);
+        furnituresElement.appendChild(furnitureElement);
+    }
+
+    // Add remove-handlers for our dogs
+}
+
+
 // Create new furniture-object and return it 
 function createNewFurniture (name, designer, year, country){
     let furniture = {
@@ -19,5 +51,8 @@ function addFurnitureToDatabase (database, furniture){
 
 // Removes a furniture based on its name from the database
 function removeFurnitureByName (){
-    
+
 }
+
+// Global values to initialize the page
+renderFurnitures(database);
