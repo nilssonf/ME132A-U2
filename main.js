fa43d33c0c43b63d330a1446322847b333019ee0
Fanny Nilsson <fannyy.nilsson@gmail.com>
2022-02-28 11:53:12 +0100

    function start

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+// Create new furniture-object and return it 
+function createNewFurniture (name, designer, year, country){
+    let furniture = {
+        name: name,
+        designer: designer,
+        year: year,
+        country: country,
+    };
+
+    return furniture;
+}
+
+// Adds the new furniture to the database
+function addFurnitureToDatabase (database, furniture){
+    database.push(furniture);
+}
+
+// Removes a furniture based on its name from the database
+function removeFurnitureByName (){
+    
+}
